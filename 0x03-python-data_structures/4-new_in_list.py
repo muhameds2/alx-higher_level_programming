82482820d24999c89533c1168cdb9625a930c48a
muhamed <vy@outlook.cl>
2024-03-18 09:27:33 -0700

    h

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    else:
+        nwlist = []
+        for i in my_list:
+            nwlist.append(i)
+        nwlist[idx] = element
+        return nwlist
