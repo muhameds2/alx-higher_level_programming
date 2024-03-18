41734284be62e11ede44affaa96bef058a195e3a
muhamed <vy@outlook.cl>
2024-03-18 16:38:09 -0700

    h

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    rvls = list(reversed(my_list))
-    for i in rvls:
-        print("{:d}".format(i))
+    if my_list == []:
+        return
+    else:
+        for i in reversed(my_list):
+            print("{:d}".format(i))
