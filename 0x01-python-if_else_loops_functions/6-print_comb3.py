d98d68d4332f19f675a14107b7fb1d6c8358b224
muhamed <vy@outlook.cl>
2024-04-15 22:53:44 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 
 for i in range(10):
     for x in range(10):
-        if i < x and x != i + 1:
-            print("{}{}".format(i, x), end=", ")
-        elif i < x and i == 8:
+        if i < x and i == 8:
             print("{}{}".format(i, x))
+        elif i < x:
+            print("{}{}".format(i, x), end=", ")
