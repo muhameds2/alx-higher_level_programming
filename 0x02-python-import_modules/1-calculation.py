ea8cb0cc5b8fa27107dcd3bb2a69c0b222e54d2d
muhamed <vy@outlook.cl>
2024-04-21 09:07:58 -0700

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,4 +5,5 @@ if __name__ == "__main__":
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}\n{} / {} = {}".format(a, b, mul(a, b), a, b, div(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
