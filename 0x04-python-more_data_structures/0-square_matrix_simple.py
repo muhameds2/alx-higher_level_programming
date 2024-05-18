1f2967885a114a87d5aaf7da57edaa6b9a7531d1
muhamed <vy@outlook.cl>
2024-05-17 18:09:48 -0700

    0-square_matrix_simple.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+def square_matrix_simple(matrix=[]):
+    nw_matrix = []
+    for row in range(len(matrix)):
+        nw_row = []
+        for column in range(len(matrix[row])):
+            nw_row.append(matrix[row][column] ** 2)
+        nw_matrix.append(nw_row)
+    return nw_matrix
